77e5def55850f4ece600bb5c9582b933168ec5ae
kuro-neko-xyz <a.aleman.h@icloud.com>
2026-04-25 20:14:48 -0600

    Closes #4

--- install.spec.sql
+++ install.spec.sql
@@ -5,3 +5,4 @@ INSERT INTO threads(id, board_uri, subject) VALUES('31f65135-d6e6-410d-b66c-dbff
 INSERT INTO posts(thread_id, body, ip_address) VALUES ('31f65135-d6e6-410d-b66c-dbffa3b9f9e6', 'First Post!', '192.168.0.1');
 INSERT INTO posts(thread_id, body, ip_address) VALUES ('31f65135-d6e6-410d-b66c-dbffa3b9f9e6', 'Second Post!!', '192.168.0.1');
 INSERT INTO posts(thread_id, body, image_url, ip_address) VALUES ('31f65135-d6e6-410d-b66c-dbffa3b9f9e6', 'Third Post with an Image!!', 'https:localhost/', '192.168.0.1');
+INSERT INTO posts(thread_id, body, ip_address) VALUES ('31f65135-d6e6-410d-b66c-dbffa3b9f9e6', 'Fourth Post but Different Author', '192.168.0.2');
